493c1aec455a63061f41b77f27ec08969814b275
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-09-15 18:13:23 +0300

    add handleNav function to mobile menu links

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
   return (
     <div className='fixed top-0 left-0 w-full z-10 ease-in duration-300 mb-10'>
         <div className='flex justify-between items-center p-4 text-white max-w-[1240px] m-auto'>
-        <h1 className= "font-ubuntu font-bold text-4xl">tindog</h1>
+        <h1 className= "font-ubuntu font-bold text-4xl"><Link href="/">tindog</Link></h1>
         
             <ul className='hidden md:flex '>
                 <li className='p-3'>
@@ -37,13 +37,13 @@ function Navbar() {
        {/*Mobile menu*/}
        <div className={nav? 'md:hidden absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center w-full h-screen bg-black ease-in duration-300' : 'md:hidden absolute top-0 bottom-0 left-[100%] right-0 flex items-center justify-center w-full h-screen bg-black ease-in duration-300'}>
        <ul className = "text-xl">
-                <li className='my-2 hover:text-grey-500'>
+                <li onClick={handleNav} className='my-2 hover:text-grey-500'>
                     <Link href="/contacts">Contacts</Link>
                 </li>
-                <li className='my-2 hover:text-grey-500'>
+                <li onClick = {handleNav} className='my-2 hover:text-grey-500'>
                     <Link href="/pricing">Pricing</Link>
                 </li>
-                <li className='my-2 hover:text-grey-500'>
+                <li onClick = {handleNav} className='my-2 hover:text-grey-500'>
                     <Link href="/downloads">Downloads</Link>
                 </li>
             </ul>
