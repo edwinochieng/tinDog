727d6721b41dc984a515671d6a6464dba021ee10
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-09-15 18:41:38 +0300

    add downloads page

---
+++ pages/downloads.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Downloads from '../components/Downloads'
+
+function downloads() {
+  return (
+    <div className='h-screen pt-24 bg-rose-600'>
+        <Downloads/>
+    </div>
+  )
+}
+
+export default downloads
