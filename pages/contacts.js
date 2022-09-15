dfba64aac6edde7ee62bd4bbeaffa2d38c5b6fda
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-09-15 19:15:48 +0300

    add contacts page

---
+++ pages/contacts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Contacts from '../components/Contacts'
+
+function contacts() {
+  return (
+    <div className='pt-8 h-screen bg-gray-200'>
+        <Contacts/>
+    </div>
+  )
+}
+
+export default contacts
